e89b229818092d0d2d50a1683c5779b1acbf7937
guptavtalent <gupta.vtalent@gmail.com>
2019-12-20 07:41:49 +0530

    Business Access Layer Created and Operation Class Ceated.One Additon Method Added..

---
+++ TestDemo/BusinessAccessLayer/Operations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestDemo.BusinessAccessLayer
+{
+    public class Operations
+    {
+        public int  Addition()
+        {
+            int c;
+            int a = 10;
+            int b = 20;
+            return   c = a + b;
+
+
+        }
+    }
+}
